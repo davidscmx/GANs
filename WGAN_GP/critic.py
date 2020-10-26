a72c75cd246dc018b779078da3e8f33accbc67a1
David Sosa <david.sosa@protonmail.com>
2020-10-26 13:38:11 +0100

    Added Spectral Normalization GAN

--- WGAN_GP/critic.py
+++ WGAN_GP/critic.py
@@ -53,3 +53,55 @@ class Critic(nn.Module):
         '''
         crit_pred = self.crit(image)
         return crit_pred.view(len(crit_pred), -1)
+
+
+class SpectralNormDiscriminator(nn.Module):
+    '''
+    Discriminator Class
+    Values:
+    im_chan: the number of channels of the output image, a scalar
+            MNIST is black-and-white (1 channel), so that's our default.
+    hidden_dim: the inner dimension, a scalar
+    '''
+
+    def __init__(self, im_chan=1, hidden_dim=16):
+        super(SpectralNormDiscriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self.make_disc_block(im_chan, hidden_dim),
+            self.make_disc_block(hidden_dim, hidden_dim * 2),
+            self.make_disc_block(hidden_dim * 2, 1, final_layer=True),
+        )
+
+    def make_disc_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a discriminator block of the DCGAN, 
+        corresponding to a convolution, a batchnorm (except for in the last layer), and an activation
+        Parameters:
+        input_channels: how many channels the input feature representation has
+        output_channels: how many channels the output feature representation should have
+        kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+        stride: the stride of the convolution
+        final_layer: whether we're on the final layer (affects activation and batchnorm)
+        '''
+        
+        # Build the neural block
+        if not final_layer:
+            return nn.Sequential(
+                nn.utils.spectral_norm(nn.Conv2d(input_channels, output_channels, kernel_size, stride)),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+        else: # Final Layer
+            return nn.Sequential(
+                nn.utils.spectral_norm(nn.Conv2d(input_channels, output_channels, kernel_size, stride)),
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the Discriminator: Given an image tensor, 
+        returns a 1-dimension tensor representing fake/real.
+        Parameters:
+        image: a flattened image tensor with dimension (im_dim)
+        '''
+        disc_pred = self.disc(image)
+        return disc_pred.view(len(disc_pred), -1)
--- WGAN_GP/wcgan.py
+++ WGAN_GP/wcgan.py
@@ -8,35 +8,15 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 from torchvision.utils import make_grid
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 torch.manual_seed(0) # Set for testing purposes, please do not change!
+import matplotlib.pyplot as plt
+
 
 from generator import Generator, get_noise
 from critic import Critic
 from losses import *
 from gradient_penalty import *
-
-def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
-    '''
-    Function for visualizing images: Given a tensor of images, number of images, and
-    size per image, plots and prints the images in an uniform grid.
-    '''
-    image_tensor = (image_tensor + 1) / 2
-    image_unflat = image_tensor.detach().cpu()
-    image_grid = make_grid(image_unflat[:num_images], nrow=5)
-    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
-    plt.show()
-
-def make_grad_hook():
-    '''
-    Function to keep track of gradients for visualization purposes, 
-    which fills the grads list when using model.apply(grad_hook).
-    '''
-    grads = []
-    def grad_hook(m):
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-            grads.append(m.weight.grad)
-    return grads, grad_hook
+from utilities import *
 
 n_epochs = 100
 z_dim = 64
@@ -63,18 +43,6 @@ gen = Generator(z_dim).to(device)
 gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
 crit = Critic().to(device) 
 crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
-
-def weights_init(m):
-    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-    if isinstance(m, nn.BatchNorm2d):
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-        torch.nn.init.constant_(m.bias, 0)
-gen = gen.apply(weights_init)
-crit = crit.apply(weights_init)
-
-
-import matplotlib.pyplot as plt
 
 cur_step = 0
 generator_losses = []
