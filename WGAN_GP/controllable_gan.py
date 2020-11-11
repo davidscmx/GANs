992ffeea66130e1c169697f96fd17f5aefd6fb79
David Sosa <david.sosa@protonmail.com>
2020-11-11 09:06:28 +0100

    Improved code organization. Added 1 unit test

--- WGAN_GP/controllable_gan.py
+++ WGAN_GP/controllable_gan.py
@@ -19,17 +19,6 @@ z_dim = 64
 batch_size = 128
 device = 'cuda'
 
-def show_tensor_images(image_tensor, num_images=16, size=(3, 64, 64), nrow=3):
-    '''
-    Function for visualizing images: Given a tensor of images, number of images, and
-    size per image, plots and prints the images in an uniform grid.
-    '''
-    image_tensor = (image_tensor + 1) / 2
-    image_unflat = image_tensor.detach().cpu()
-    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
-    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
-    plt.show()
-    
 class Generator(nn.Module):
     '''
     Generator Class
@@ -84,17 +73,6 @@ class Generator(nn.Module):
         '''
         x = noise.view(len(noise), self.z_dim, 1, 1)
         return self.gen(x)
-
-def get_noise(n_samples, z_dim, device='cpu'):
-    '''
-    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
-    creates a tensor of that shape filled with random numbers from the normal distribution.
-    Parameters:
-        n_samples: the number of samples in the batch, a scalar
-        z_dim: the dimension of the noise vector, a scalar
-        device: the device type
-    '''
-    return torch.randn(n_samples, z_dim, device=device)
 
 def train_classifier(filename):
     import seaborn as sns
@@ -158,8 +136,6 @@ def train_classifier(filename):
                 torch.save({"classifier": classifier.state_dict()}, filename)
             cur_step += 1
 
-# Uncomment the last line to train your own classfier - this line will not work in Coursera.
-# If you'd like to do this, you'll have to download it and run it, ideally using a GPU 
 # train_classifier("filename")
 
 import torch
@@ -176,45 +152,6 @@ classifier.eval()
 print("Loaded the models!")
 
 opt = torch.optim.Adam(classifier.parameters(), lr=0.01)
-
-def calculate_updated_noise(noise, weight):
-    '''
-    Function to return noise vectors updated with stochastic gradient ascent.
-    Parameters:
-        noise: the current noise vectors. You have already called the backwards function on the target class
-          so you can access the gradient of the output class with respect to the noise by using noise.grad
-        weight: the scalar amount by which you should weight the noise gradient
-    '''
-    new_noise = noise + (noise.grad*weight)
-    return new_noise
-
-# UNIT TEST
-# Check that the basic function works
-opt.zero_grad()
-noise = torch.ones(20, 20) * 2
-noise.requires_grad_()
-fake_classes = (noise ** 2).mean()
-fake_classes.backward()
-new_noise = calculate_updated_noise(noise, 0.1)
-assert type(new_noise) == torch.Tensor
-assert tuple(new_noise.shape) == (20, 20)
-assert new_noise.max() == 2.0010
-assert new_noise.min() == 2.0010
-assert torch.isclose(new_noise.sum(), torch.tensor(0.4) + 20 * 20 * 2)
-print("Success!")
-
-# Check that it works for generated images
-opt.zero_grad()
-noise = get_noise(32, z_dim).to(device).requires_grad_()
-fake = gen(noise)
-fake_classes = classifier(fake)[:, 0]
-fake_classes.mean().backward()
-noise.data = calculate_updated_noise(noise, 0.01)
-fake = gen(noise)
-fake_classes_new = classifier(fake)[:, 0]
-assert torch.all(fake_classes_new > fake_classes)
-print("Success!")
-
 
 # First generate a bunch of images with the generator
 n_images = 8
@@ -243,58 +180,6 @@ for i in range(grad_steps):
 
 plt.rcParams['figure.figsize'] = [n_images * 2, grad_steps * 2]
 show_tensor_images(torch.cat(fake_image_history[::skip], dim=2), num_images=n_images, nrow=n_images)
-
-# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
-# GRADED FUNCTION: get_score
-def get_score(current_classifications, original_classifications, target_indices, other_indices, penalty_weight):
-    '''
-    Function to return the score of the current classifications, penalizing changes
-    to other classes with an L2 norm.
-    Parameters:
-        current_classifications: the classifications associated with the current noise
-        original_classifications: the classifications associated with the original noise
-        target_indices: the index of the target class
-        other_indices: the indices of the other classes
-        penalty_weight: the amount that the penalty should be weighted in the overall score
-    '''
-    # Steps: 1) Calculate the change between the original and current classifications (as a tensor)
-    #           by indexing into the other_indices you're trying to preserve, like in x[:, features].
-    #        2) Calculate the norm (magnitude) of changes per example.
-    #        3) Multiply the mean of the example norms by the penalty weight. 
-    #           This will be your other_class_penalty.
-    #           Make sure to negate the value since it's a penalty!
-    #        4) Take the mean of the current classifications for the target feature over all the examples.
-    #           This mean will be your target_score.
-    
-    # Calculate the norm (magnitude) of changes per example and multiply by penalty weight
-    change_other = current_classifications[:,other_indices] - original_classifications[:,other_indices] 
-    other_class_penalty = -torch.mean(torch.norm(change_other, dim=1))*penalty_weight
-    # Take the mean of the current classifications for the target feature
-    target_score = torch.mean(current_classifications[:,target_indices])
-    return target_score + other_class_penalty
-
-
-# UNIT TEST
-assert torch.isclose(
-    get_score(torch.ones(4, 3), torch.zeros(4, 3), [0], [1, 2], 0.2), 
-    1 - torch.sqrt(torch.tensor(2.)) * 0.2
-)
-rows = 10
-current_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
-original_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
-
-# Must be 3
-assert get_score(current_class, original_class, [1, 3] , [0, 2], 0.2).item() == 3
-
-current_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
-original_class = torch.tensor([[4] * rows, [4] * rows, [2] * rows, [1] * rows]).T.float()
-
-# Must be 3 - 0.2 * sqrt(10)
-assert torch.isclose(get_score(current_class, original_class, [1, 3] , [0, 2], 0.2), 
-                     -torch.sqrt(torch.tensor(10.0)) * 0.2 + 3)
-
-print("Success!")
-
 
 fake_image_history = []
 ### Change me! ###
--- WGAN_GP/test_utilities.py
+++ WGAN_GP/test_utilities.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from utilities import *
 import unittest
+from classifier import *
 
 class test_utilities(unittest.TestCase):
     def test_get_gen_loss(self):
@@ -24,4 +25,52 @@ class test_utilities(unittest.TestCase):
         gen_dim, disc_dim = get_input_dimensions(23, (12, 23, 52), 9)
         self.assertEqual(gen_dim,32)
         self.assertEqual(disc_dim,21)
+    
+    def test_calculate_updated_noise_basic(self):
+        device = 'cuda'
+        label_indices = range(40)
+        classifier = Classifier(n_classes=len(label_indices)).to(device)
+        opt = torch.optim.Adam(classifier.parameters(), lr=0.01)
+        opt.zero_grad()
+        noise = torch.ones(20, 20) * 2
+        noise.requires_grad_()
+        fake_classes = (noise ** 2).mean()
+        fake_classes.backward()
+        new_noise = calculate_updated_noise(noise, 0.1)
+        self.assertEqual(type(new_noise),torch.Tensor)
+        self.assertEqual(tuple(new_noise.shape),(20, 20))
+        self.assertEqual(new_noise.max(),2.0010)
+        self.assertEqual(new_noise.min(),2.0010)
+        self.assertTrue(torch.isclose(new_noise.sum(), torch.tensor(0.4) + 20 * 20 * 2))
         
+    #def test_calculate_updated_noise_generated(self):
+    #    # Check that it works for generated images
+    #    opt.zero_grad()
+    #    noise = get_noise(32, z_dim).to(device).requires_grad_()
+    #    fake = gen(noise)
+    #    fake_classes = classifier(fake)[:, 0]
+    #    fake_classes.mean().backward()
+    #    noise.data = calculate_updated_noise(noise, 0.01)
+    #    fake = gen(noise)
+    #    fake_classes_new = classifier(fake)[:, 0]
+    #    assert torch.all(fake_classes_new > fake_classes)
+#
+    #def test_get_score():
+    #    # UNIT TEST
+    #    assert torch.isclose(
+    #        get_score(torch.ones(4, 3), torch.zeros(4, 3), [0], [1, 2], 0.2), 
+    #        1 - torch.sqrt(torch.tensor(2.)) * 0.2
+    #    )
+    #    rows = 10
+    #    current_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
+    #    original_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
+    #    
+    #    # Must be 3
+    #    assert get_score(current_class, original_class, [1, 3] , [0, 2], 0.2).item() == 3
+    #    
+    #    current_class = torch.tensor([[1] * rows, [2] * rows, [3] * rows, [4] * rows]).T.float()
+    #    original_class = torch.tensor([[4] * rows, [4] * rows, [2] * rows, [1] * rows]).T.float()
+    #    
+    #    # Must be 3 - 0.2 * sqrt(10)
+    #    assert torch.isclose(get_score(current_class, original_class, [1, 3] , [0, 2], 0.2), 
+    #                 -torch.sqrt(torch.tensor(10.0)) * 0.2 + 3)
