01c435fd2235deb1066be2c44ddfaf6dfa680de8
David Sosa <david.sosa@protonmail.com>
2020-12-28 15:28:18 +0100

    Added two cycle GAN losses

--- WGAN_GP/building_blocks/losses.py
+++ WGAN_GP/building_blocks/losses.py
@@ -26,3 +26,47 @@ def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
     '''
     crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred)  + c_lambda*gp
     return crit_loss
+
+### Cycle GAN Losses ###
+def get_disc_loss_cycle_gan(real_X, fake_X, disc_X, adv_criterion):
+    '''
+    Return the loss of the discriminator given inputs.
+    Parameters:
+        real_X: the real images from pile X
+        fake_X: the generated images of class X
+        disc_X: the discriminator for class X; takes images and returns real/fake class X
+            prediction matrices
+        adv_criterion: the adversarial loss function; takes the discriminator 
+            predictions and the target labels and returns a adversarial 
+            loss (which you aim to minimize). Usually adv_criterion = nn.MSELoss() 
+    '''
+    
+    out_fake = disc_X(fake_X)
+    out_real = disc_X(real_X)        
+    disc_fake_loss = adv_criterion(out_fake, torch.zeros_like(out_fake))
+    disc_real_loss = adv_criterion(out_real, torch.ones_like(out_real))    
+    disc_loss = (disc_fake_loss + disc_real_loss) / 2
+    return disc_loss
+
+def get_gen_adversarial_loss_cycle_gan(real_X, disc_Y, gen_XY, adv_criterion):
+    '''
+    Return the adversarial loss of the generator given inputs
+    (and the generated images for testing purposes).
+    Parameters:
+        real_X: the real images from pile X
+        disc_Y: the discriminator for class Y; takes images and returns real/fake class Y
+            prediction matrices
+        gen_XY: the generator for class X to Y; takes images and returns the images 
+            transformed to class Y
+        adv_criterion: the adversarial loss function; takes the discriminator 
+                  predictions and the target labels and returns a adversarial 
+                  loss (which you aim to minimize). Usually adv_criterion = nn.MSELoss() 
+    '''
+    
+    disc_real = disc_Y(real_X)    
+    fake_Y = gen_XY(real_X)   
+    disc_fake_Y = disc_Y(fake_Y)
+        
+    adversarial_loss = adv_criterion(disc_fake_Y, torch.ones_like(disc_fake_Y))
+    
+    return adversarial_loss, fake_Y
--- WGAN_GP/tests/test_losses.py
+++ WGAN_GP/tests/test_losses.py
@@ -1,5 +1,9 @@
 import torch
 from torch import nn
+
+import sys
+sys.path.append("../building_blocks/")
+
 from losses import *
 import unittest
 
@@ -11,6 +15,33 @@ class test_losses(unittest.TestCase):
     def test_get_critic_loss(self):
         self.assertTrue(torch.isclose(get_crit_loss(torch.tensor(1.), torch.tensor(2.), torch.tensor(3.), 0.1),torch.tensor(-0.7)))
         self.assertTrue(torch.isclose(get_crit_loss(torch.tensor(20.), torch.tensor(-20.), torch.tensor(2.), 10),torch.tensor(60.)))
+
+    def test_get_disc_loss_cycle_gan(self):
+        test_disc_X = lambda x: x * 97
+        test_real_X = torch.tensor(83.)
+        test_fake_X = torch.tensor(89.)
+        test_adv_criterion = lambda x, y: x * 79 + y * 73
+
+        self.assertLess(torch.abs((get_disc_loss_cycle_gan(test_real_X, test_fake_X, test_disc_X, test_adv_criterion)) - 659054.5000), 1e-6)
+        test_disc_X = lambda x: x.mean(0, keepdim=True)
+        test_adv_criterion = torch.nn.BCEWithLogitsLoss()
+        test_input = torch.ones(20, 10)
+        # If this runs, it's a pass - checks that the shapes are treated correctly
+        get_disc_loss_cycle_gan(test_input, test_input, test_disc_X, test_adv_criterion)
+
+    def test_get_gen_loss_cycle_gan(self):
+        test_disc_Y = lambda x: x * 97
+        test_real_X = torch.tensor(83.)
+        test_gen_XY = lambda x: x * 89
+        test_adv_criterion = lambda x, y: x * 79 + y * 73
+        test_res = get_gen_adversarial_loss_cycle_gan(test_real_X, test_disc_Y, test_gen_XY, test_adv_criterion)
+        self.assertLess(torch.abs(test_res[0] - 56606652), 1e-6)
+        self.assertLess( torch.abs(test_res[1] - 7387), 1e-6)
+        test_disc_Y = lambda x: x.mean(0, keepdim=True)
+        test_adv_criterion = torch.nn.BCEWithLogitsLoss()
+        test_input = torch.ones(20, 10)
+        # If this runs, it's a pass - checks that the shapes are treated correctly
+        get_gen_adversarial_loss_cycle_gan(test_input, test_disc_Y, test_gen_XY, test_adv_criterion)
 
 if __name__ == '__main__':
     unittest.main()
