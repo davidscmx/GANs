b998f14d0f917bb799ecb5c475baee22cce08ae4
David Sosa <david.sosa@protonmail.com>
2020-12-30 10:12:18 +0100

    Added all cycle GAN losses with unit tests

--- WGAN_GP/building_blocks/losses.py
+++ WGAN_GP/building_blocks/losses.py
@@ -70,3 +70,85 @@ def get_gen_adversarial_loss_cycle_gan(real_X, disc_Y, gen_XY, adv_criterion):
     adversarial_loss = adv_criterion(disc_fake_Y, torch.ones_like(disc_fake_Y))
     
     return adversarial_loss, fake_Y
+
+def get_identity_loss(real_X, gen_YX, identity_criterion):
+    '''
+    Return the identity loss of the generator given inputs
+    (and the generated images for testing purposes).
+    Parameters:
+        real_X: the real images from pile X
+        gen_YX: the generator for class Y to X; takes images and returns the images 
+            transformed to class X
+        identity_criterion: the identity loss function; takes the real images from X and
+                        those images put through a Y->X generator and returns the identity 
+                        loss (which you aim to minimize)
+    '''
+    
+    identity_X = gen_YX(real_X)
+    identity_loss = identity_criterion(real_X, identity_X)
+    
+    return identity_loss, identity_X\
+
+def get_cycle_consistency_loss(real_X, fake_Y, gen_YX, cycle_criterion):
+    '''
+    Return the cycle consistency loss of the generator given inputs
+    (and the generated images for testing purposes).
+    Parameters:
+        real_X: the real images from pile X
+        fake_Y: the generated images of class Y
+        gen_YX: the generator for class Y to X; takes images and returns the images 
+            transformed to class X
+        cycle_criterion: the cycle consistency loss function; takes the real images from X and
+                        those images put through a X->Y generator and then Y->X generator
+                        and returns the cycle consistency loss (which you aim to minimize)
+    '''
+    cycle_X = gen_YX(fake_Y)
+    cycle_loss = cycle_criterion(real_X, cycle_X)
+
+    return cycle_loss, cycle_X
+
+def get_total_gen_loss_cycle_gan(real_A, real_B, gen_AB, gen_BA, disc_A, disc_B, adv_criterion, 
+                                 identity_criterion, cycle_criterion, lambda_identity=0.1, 
+                                 lambda_cycle=10):
+    '''
+    Return the loss of the generator given inputs.
+    Parameters:
+        real_A: the real images from pile A
+        real_B: the real images from pile B
+        gen_AB: the generator for class A to B; takes images and returns the images 
+            transformed to class B
+        gen_BA: the generator for class B to A; takes images and returns the images 
+            transformed to class A
+        disc_A: the discriminator for class A; takes images and returns real/fake class A
+            prediction matrices
+        disc_B: the discriminator for class B; takes images and returns real/fake class B
+            prediction matrices
+        adv_criterion: the adversarial loss function; takes the discriminator 
+            predictions and the true labels and returns a adversarial 
+            loss (which you aim to minimize)
+        identity_criterion: the reconstruction loss function used for identity loss
+            and cycle consistency loss; takes two sets of images and returns
+            their pixel differences (which you aim to minimize)
+        cycle_criterion: the cycle consistency loss function; takes the real images from X and
+            those images put through a X->Y generator and then Y->X generator
+            and returns the cycle consistency loss (which you aim to minimize).
+            Note that in practice, cycle_criterion == identity_criterion == L1 loss
+        lambda_identity: the weight of the identity loss
+        lambda_cycle: the weight of the cycle-consistency loss
+    '''
+    adv_loss_AB, fake_B = get_gen_adversarial_loss_cycle_gan(real_A, disc_A, gen_AB, adv_criterion)    
+    adv_loss_BA, fake_A = get_gen_adversarial_loss_cycle_gan(real_B, disc_B, gen_BA, adv_criterion)
+    
+    # Identity Loss -- get_identity_loss(real_X, gen_YX, identity_criterion)
+    identity_loss_AB, _ = get_identity_loss(real_A, gen_BA, identity_criterion)
+    identity_loss_BA, _ = get_identity_loss(real_B, gen_AB, identity_criterion)
+    
+    # Cycle-consistency Loss -- get_cycle_consistency_loss(real_X, fake_Y, gen_YX, cycle_criterion)
+    cycle_consistency_loss_AB, _ = get_cycle_consistency_loss(real_A, fake_B, gen_BA, cycle_criterion)
+    cycle_consistency_loss_BA, _ = get_cycle_consistency_loss(real_B, fake_A, gen_AB, cycle_criterion)
+    
+    # Total loss
+    gen_loss = adv_loss_AB + adv_loss_BA + \
+               lambda_identity*identity_loss_AB + lambda_identity*identity_loss_BA + \
+               lambda_cycle*cycle_consistency_loss_AB + lambda_cycle*cycle_consistency_loss_BA
+    return gen_loss, fake_A, fake_B
--- WGAN_GP/tests/test_losses.py
+++ WGAN_GP/tests/test_losses.py
@@ -43,5 +43,52 @@ class test_losses(unittest.TestCase):
         # If this runs, it's a pass - checks that the shapes are treated correctly
         get_gen_adversarial_loss_cycle_gan(test_input, test_disc_Y, test_gen_XY, test_adv_criterion)
 
+    def test_identity_loss(self):
+        test_real_X = torch.tensor(83.)
+        test_gen_YX = lambda x: x * 89
+        test_identity_criterion = lambda x, y: (x + y) * 73
+        test_res = get_identity_loss(test_real_X, test_gen_YX, test_identity_criterion)
+        self.assertLess(torch.abs(test_res[0] - 545310), 1e-6)
+        self.assertLess(torch.abs(test_res[1] - 7387), 1e-6)
+
+    def test_cycle_consistency_loss(self):
+        test_real_X = torch.tensor(83.)
+        test_fake_Y = torch.tensor(97.)
+        test_gen_YX = lambda x: x * 89
+        test_cycle_criterion = lambda x, y: (x + y) * 73
+        test_res = get_cycle_consistency_loss(test_real_X, test_fake_Y, test_gen_YX, test_cycle_criterion)
+        self.assertLess(torch.abs(test_res[1] - 8633), 1e-6)
+        self.assertLess(torch.abs(test_res[0] - 636268), 1e-6)
+    
+    def test_get_total_gen_loss_cycle_gan(self):
+
+        test_real_A = torch.tensor(97)
+        test_real_B = torch.tensor(89)
+        test_gen_AB = lambda x: x * 83
+        test_gen_BA = lambda x: x * 79
+        test_disc_A = lambda x: x * 47
+        test_disc_B = lambda x: x * 43
+        test_adv_criterion = lambda x, y: x * 73 + y * 71
+        test_recon_criterion = lambda x, y: (x + y) * 61
+        test_lambda_identity = 59
+        test_lambda_cycle = 53
+        
+        test_res = get_total_gen_loss_cycle_gan(
+        test_real_A, 
+        test_real_B, 
+        test_gen_AB, 
+        test_gen_BA, 
+        test_disc_A,
+        test_disc_B,
+        test_adv_criterion, 
+        test_recon_criterion, 
+        test_recon_criterion, 
+        test_lambda_identity, 
+        test_lambda_cycle)
+
+        self.assertEqual(test_res[0].item(), 4048102400)
+        self.assertEqual(test_res[1].item(), 7031)
+        self.assertEqual(test_res[2].item(), 8051)
+
 if __name__ == '__main__':
     unittest.main()
