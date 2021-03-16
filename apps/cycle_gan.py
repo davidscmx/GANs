526cc7b85150c6b204a3cd104807b1a7786c5f2c
David Sosa <david.sosa@protonmail.com>
2021-03-16 14:46:19 +0100

    Readded cycle gan script

---
+++ apps/cycle_gan.py
@@ -0,0 +1,176 @@
+import glob
+import random
+import os
+
+from PIL import Image
+import torch.nn.functional as F
+
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+import sys
+sys.path.append("../building_blocks/")
+from utilities import *
+from losses import *
+from generator_cycle_gan import GeneratorCycleGan
+from discriminator_cycle_gan import DiscriminatorCycleGan
+
+from skimage import color
+import numpy as np
+plt.rcParams["figure.figsize"] = (10, 10)
+from tqdm.auto import tqdm
+
+# Inspired by https://github.com/aitorzip/PyTorch-CycleGAN/blob/master/datasets.py
+class ImageDataset(Dataset):
+    def __init__(self, root, transform=None, mode='train'):
+        self.transform = transform
+        self.files_A = sorted(glob.glob(os.path.join(root, '%sA' % mode) + '/*.*'))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%sB' % mode) + '/*.*'))
+        if len(self.files_A) > len(self.files_B):
+            self.files_A, self.files_B = self.files_B, self.files_A
+        self.new_perm()
+        assert len(self.files_A) > 0, "Make sure you downloaded the horse2zebra images!"
+
+    def new_perm(self):
+        self.randperm = torch.randperm(len(self.files_B))[:len(self.files_A)]
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        item_B = self.transform(Image.open(self.files_B[self.randperm[index]]))
+        if item_A.shape[0] != 3: 
+            item_A = item_A.repeat(3, 1, 1)
+        if item_B.shape[0] != 3: 
+            item_B = item_B.repeat(3, 1, 1)
+        if index == len(self) - 1:
+            self.new_perm()
+        # Old versions of PyTorch didn't support normalization for different-channeled images
+        return (item_A - 0.5) * 2, (item_B - 0.5) * 2
+
+    def __len__(self):
+        return min(len(self.files_A), len(self.files_B))
+
+adv_criterion = nn.MSELoss() 
+recon_criterion = nn.L1Loss() 
+
+
+n_epochs = 20
+dim_A = 3
+dim_B = 3
+display_step = 200
+batch_size = 1
+lr = 0.0001
+load_shape = 286
+target_shape = 128
+device = 'cuda'
+
+transform = transforms.Compose([
+    transforms.Resize(load_shape),
+    transforms.RandomCrop(target_shape),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+
+dataset = ImageDataset("/media/david/mem-pc-2TB/GAN_framework/datasets/horse2zebra", transform=transform)
+
+gen_AB = GeneratorCycleGan(dim_A, dim_B).to(device)
+gen_BA = GeneratorCycleGan(dim_B, dim_A).to(device)
+gen_opt = torch.optim.Adam(list(gen_AB.parameters()) + list(gen_BA.parameters()), lr=lr, betas=(0.5, 0.999))
+disc_A = DiscriminatorCycleGan(dim_A).to(device)
+disc_A_opt = torch.optim.Adam(disc_A.parameters(), lr=lr, betas=(0.5, 0.999))
+disc_B = DiscriminatorCycleGan(dim_B).to(device)
+disc_B_opt = torch.optim.Adam(disc_B.parameters(), lr=lr, betas=(0.5, 0.999))
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+
+# Feel free to change pretrained to False if you're training the model from scratch
+pretrained = False
+if pretrained:
+    pre_dict = torch.load('/home/david/onlineLearning/GAN_framework/WGAN_GP/models/horse2zebra.pth')
+    print(pre_dict.keys())
+    gen_AB.load_state_dict(pre_dict['gen_AB'])
+    gen_BA.load_state_dict(pre_dict['gen_BA'])
+    gen_opt.load_state_dict(pre_dict['gen_opt'])
+    disc_A.load_state_dict(pre_dict['disc_A'])
+    disc_A_opt.load_state_dict(pre_dict['disc_A_opt'])
+    disc_B.load_state_dict(pre_dict['disc_B'])
+    disc_B_opt.load_state_dict(pre_dict['disc_B_opt'])
+else:
+    gen_AB = gen_AB.apply(weights_init)
+    gen_BA = gen_BA.apply(weights_init)
+    disc_A = disc_A.apply(weights_init)
+    disc_B = disc_B.apply(weights_init)
+
+
+
+def train(save_model=False):
+    mean_generator_loss = 0
+    mean_discriminator_loss = 0
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    cur_step = 0
+
+    for epoch in range(n_epochs):
+        # Dataloader returns the batches
+        # for image, _ in tqdm(dataloader):
+        for real_A, real_B in tqdm(dataloader):
+            real_A = nn.functional.interpolate(real_A, size=target_shape)
+            real_B = nn.functional.interpolate(real_B, size=target_shape)
+            cur_batch_size = len(real_A)
+            real_A = real_A.to(device)
+            real_B = real_B.to(device)
+
+            ### Update discriminator A ###
+            disc_A_opt.zero_grad() # Zero out the gradient before backpropagation
+            with torch.no_grad():
+                fake_A = gen_BA(real_B)
+            disc_A_loss = get_disc_loss_cycle_gan(real_A, fake_A, disc_A, adv_criterion)
+            disc_A_loss.backward(retain_graph=True) # Update gradients
+            disc_A_opt.step() # Update optimizer
+
+            ### Update discriminator B ###
+            disc_B_opt.zero_grad() # Zero out the gradient before backpropagation
+            with torch.no_grad():
+                fake_B = gen_AB(real_A)
+            disc_B_loss = get_disc_loss_cycle_gan(real_B, fake_B, disc_B, adv_criterion)
+            disc_B_loss.backward(retain_graph=True) # Update gradients
+            disc_B_opt.step() # Update optimizer
+
+            ### Update generator ###
+            gen_opt.zero_grad()
+            gen_loss, fake_A, fake_B = get_total_gen_loss_cycle_gan(
+                real_A, real_B, gen_AB, gen_BA, disc_A, disc_B, adv_criterion, recon_criterion, recon_criterion
+            )
+            gen_loss.backward() # Update gradients
+            gen_opt.step() # Update optimizer
+
+            # Keep track of the average discriminator loss
+            mean_discriminator_loss += disc_A_loss.item() / display_step
+            # Keep track of the average generator loss
+            mean_generator_loss += gen_loss.item() / display_step
+
+            ### Visualization code ###
+            if cur_step % display_step == 0:
+                print(f"Epoch {epoch}: Step {cur_step}: Generator (U-Net) loss: {mean_generator_loss}, Discriminator loss: {mean_discriminator_loss}")
+                show_tensor_images(torch.cat([real_A, real_B]), size=(dim_A, target_shape, target_shape))
+                show_tensor_images(torch.cat([fake_B, fake_A]), size=(dim_B, target_shape, target_shape))
+                mean_generator_loss = 0
+                mean_discriminator_loss = 0
+                # You can change save_model to True if you'd like to save the model
+                if save_model:
+                    torch.save({
+                        'gen_AB': gen_AB.state_dict(),
+                        'gen_BA': gen_BA.state_dict(),
+                        'gen_opt': gen_opt.state_dict(),
+                        'disc_A': disc_A.state_dict(),
+                        'disc_A_opt': disc_A_opt.state_dict(),
+                        'disc_B': disc_B.state_dict(),
+                        'disc_B_opt': disc_B_opt.state_dict()
+                    }, f"cycleGAN_{cur_step}.pth")
+            cur_step += 1
+train()
